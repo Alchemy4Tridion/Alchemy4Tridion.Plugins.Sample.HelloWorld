386cd81035db34122e46690252f99141c30e3df8
alex-klock <alex-klock@users.noreply.github.com>
2015-06-15 21:59:00 -0400

    Initial Commit!
    Initial commit of the sample project.

---
+++ Alchemy4Tridion.Plugins.Sample.HelloWorld/Config/HelloResourceGroup.cs
@@ -0,0 +1,34 @@
+﻿using Alchemy4Tridion.Plugins.GUI.Configuration;
+using Alchemy4Tridion.Plugins.GUI.Configuration.Elements;
+
+namespace Alchemy4Tridion.Plugins.Sample.HelloWorld.Config
+{
+    /// <summary>
+    /// Represents the ResourceGroup element within the editor configuration that contains this plugin's files
+    /// and references.
+    /// </summary>
+    public class HelloResourceGroup : ResourceGroup
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public HelloResourceGroup()
+        {
+            // When adding files you only need to specify the filename and not full path
+            AddFile("ErrorTestCommand.js");
+            AddFile("GetApiVersionCommand.js");
+            AddFile("HelloWorldCommand.js");
+
+            AddFile("Hello.css");
+
+            // When referencing commandsets you can just use the generic AddFile with your CommandSet as the type.
+            AddFile<HelloCommandSet>();
+
+            // The above is just a convinient way of doing the following...
+            // AddFile(FileTypes.Reference, "Alchemy.Plugins.HelloWorld.Commands.HelloCommandSet");
+            
+            // If you want this resource group to contain the js proxies to call your webservice, call AddWebApiProxy()
+            AddWebApiProxy();
+        }
+    }
+}
